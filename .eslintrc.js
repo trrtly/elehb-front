1f0655c2059ae5e3691e91db8284c66b5afdf0c4
trrtly <328602875@qq.com>
2020-05-05 21:03:46 +0800

    fix: ignore WeixinJSBridge

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
         node: true
     },
     extends: ['plugin:vue/essential', '@vue/prettier'],
+    globals: {"WeixinJSBridge" : false},
     rules: {
         'no-console': 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
